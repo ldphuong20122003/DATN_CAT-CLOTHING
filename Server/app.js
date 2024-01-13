547182cae24258ce30778206fe72c64c43c2b9b8
darKa88888888 <127387518+darKa88888888@users.noreply.github.com>
2024-01-13 13:55:49 +0700

    full CRUD API
    Ok

--- Server/app.js
+++ Server/app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+const bodyParser = require('body-parser');
 var app = express();
 
 // view engine setup
@@ -18,6 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
