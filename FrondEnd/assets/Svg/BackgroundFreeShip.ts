bb1235d1f5dda5946ce41bfa9a6339e69769d970
ldphuong2012 <127278896+ldphuong2012@users.noreply.github.com>
2024-01-31 11:33:31 +0700

    add gd HomePage

---
+++ FrondEnd/assets/Svg/BackgroundFreeShip.ts
@@ -0,0 +1,4 @@
+export default()=>`<svg width="45" height="14" viewBox="0 0 45 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M40.4082 14L0 14L0 0L40.4082 0L45 7L40.4082 14Z" fill="#00CBD8"/>
+</svg>
+`
---
+++ FrondEnd/assets/Svg/VoucherSvg.ts
@@ -0,0 +1,10 @@
+export default()=>`<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+<rect width="24" height="24" fill="url(#pattern0)"/>
+<defs>
+<pattern id="pattern0" patternContentUnits="objectBoundingBox" width="1" height="1">
+<use xlink:href="#image0_21_2218" transform="scale(0.0416667)"/>
+</pattern>
+<image id="image0_21_2218" width="24" height="24" xlink:href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAANZJREFUSEvdlE0SwUAQRl9uwhZ3wCWsOQlOwtolcAdsuQnTqZnUJJLppKZ6QapmlZnvdX/9U2D8Fcb6/BdgB8yBRaZtF+DqNESvZtENmGaKh+d3YNYEvP3f3LrUdGKxPoCzD2KZyFQFiE2PDgENMAHEnsqdtgzkwioB6QpexE9RHUvtGCDdI2cLHIHNwIIfgLXT3Dsd6SQ5X4MWUnwB4xZAyqKnC2zkM6gsbnaMKcDcotBeZkUOAPM2TU2yNgfSF+qgma0K82Vnvq4HDm6/67l+q5TfB3wA2fY4GQ1dER4AAAAASUVORK5CYII="/>
+</defs>
+</svg>
+`
