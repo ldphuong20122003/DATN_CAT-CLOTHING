7a113ea8fd757286a03a5a1df458b4ebaecc5578
ldphuong2012 <127278896+ldphuong2012@users.noreply.github.com>
2024-02-07 09:40:46 +0700

    Format giao dien

--- FrondEnd/src/app-views/Product/ReOrder/component/ListReOder.js
+++ FrondEnd/src/app-views/Product/ReOrder/component/ListReOder.js
@@ -7,42 +7,51 @@ const ListReOder = ({ data, onPress }) => {
   };
   const _renderItem = ({ item, index }) => {
     return (
-      <View style={{ marginRight:10 }}>
+      <View style={{ marginRight: 10 }}>
         <TouchableOpacity onPress={() => pressItem(item)}>
-        <View 
-        style={{width:100,justifyContent:'center',marginTop:14}}>
-              <View style={{width:100,justifyContent:'center'}}>
-                <View>
-                  {item.image && (
-                    <Image
-                      source={{uri: item.image}}
-                      style={{
-                        height: 100,
-                        width: 100,
-                        justifyContent: 'center',
-                      }}
-                    />
-                  )}
-                </View>
+          <View style={{ width: 100, justifyContent: "center", marginTop: 14 }}>
+            <View style={{ width: 100, justifyContent: "center" }}>
+              <View>
+                {item.image && (
+                  <Image
+                    source={{ uri: item.image }}
+                    style={{
+                      height: 100,
+                      width: 100,
+                      justifyContent: "center",
+                    }}
+                  />
+                )}
+              </View>
 
-                <View style={{paddingHorizontal:6}}>
-                <View >
-                  <Text style={{ fontSize:10,fontWeight:400,color:'black'}}>
+              <View style={{ paddingHorizontal: 6 }}>
+                <View>
+                  <Text
+                    style={{ fontSize: 10, fontWeight: 400, color: "black" }}
+                  >
                     {item.name}
                   </Text>
-                </View>      
+                </View>
 
-                <View style={{flexDirection:'row',justifyContent:'space-between',marginTop:4}} >
-                  <Text style={{ fontSize:10,fontWeight:400,color:'#ef4444'}}>
+                <View
+                  style={{
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    marginTop: 4,
+                  }}
+                >
+                  <Text
+                    style={{ fontSize: 10, fontWeight: 400, color: "#ef4444" }}
+                  >
                     {item.price}
                   </Text>
-                  <Image source={require("../../../../../assets/ShoppingCart.png")}/>
+                  <Image
+                    source={require("../../../../../assets/ShoppingCart.png")}
+                  />
                 </View>
-                    
-                </View>
-              
               </View>
             </View>
+          </View>
         </TouchableOpacity>
       </View>
     );
