547636b7d68f5f6435a347d0a5f42ed5ee5d8f71
LHNghinh <nghinhlehong@gmail.com>
2024-03-13 15:31:08 +0700

    Hien thi giao dien dia chi nhan hang theo iduser

--- FrondEnd/src/app-views/Address/Address.js
+++ FrondEnd/src/app-views/Address/Address.js
@@ -1,30 +1,71 @@
-import React from "react";
-import { View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { ActivityIndicator, Button, Text, View } from "react-native";
 import ListAddress from "./component/ListAddress";
-
+import config from "../../../config";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const Address = () => {
-  const default_data = [
-    {
-      name:'Đức Phương',
-      phone_number: '0987654321',
-      address:'123 Sông Hồng , Hai Bà Trưng , Hà Nội'
-    },
-    {
-        name:'Hồng Nghinh',
-        phone_number: '0987654321',
-        address:'123 Sông Hồng , Hai Bà Trưng , Hà Nội'
-    },
-    {
-        name:'Văn Khanh',
-        phone_number: '0987654321',
-        address:'123 Sông Hồng , Hai Bà Trưng , Hà Nội'
+  const IP = config.IP;
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [userId, setUserId] = useState("");
+  const getUserId = async () => {
+    try {
+      const userIdValue = await AsyncStorage.getItem("UserId");
+      if (userIdValue !== null) {
+        setUserId(userIdValue);
+        return fetch(`http://${IP}:3000/API/users/?id=` + userIdValue)
+          .then((res) => res.json())
+          .catch((err) => console.log(err));
+      }
+    } catch (error) {
+      console.log(error);
     }
-  ];
+  };
+
+  const getAPI = async () => {
+    try {
+      setLoading(true);
+      const response = await fetch(
+        `http://${IP}:3000/API/Address/?id=` + userId
+      );
+      const data = await response.json();
+      setLoading(false);
+      setData(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getUserId();
+  }, []);
+
+  useEffect(() => {
+    if (userId) {
+      getAPI();
+    }
+  }, [userId]);
   return (
-  <View>
-     <ListAddress data={default_data}/>
-  </View>
+    <View>
+      <View>
+        <View
+          style={{
+            borderBottomWidth: 0.5,
+            borderColor: "#fff",
+            paddingBottom: 7,
+          }}
+        >
+          {loading ? (
+            <ActivityIndicator /> // Hiển thị trạng thái loading khi đang tải dữ liệu
+          ) : data.length > 0 ? (
+            <ListAddress data={data} /> // Hiển thị danh sách địa chỉ nếu có dữ liệu
+          ) : (
+            <Text>Bạn hiện chưa có địa chỉ nào</Text> // Hiển thị thông báo nếu không có dữ liệu
+          )}
+        </View>
+      </View>
+    </View>
   );
 };
 export default Address;
--- FrondEnd/src/app-views/Address/component/ListAddress.js
+++ FrondEnd/src/app-views/Address/component/ListAddress.js
@@ -1,18 +1,17 @@
+ListAddress 
 import React, { useState } from "react";
 import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
 import { SvgXml } from "react-native-svg";
 
 const ListAddress = ({ data, onPress }) => {
-  
   const [selectedItem, setSelectedItem] = useState(null);
-
   const pressItem = (item) => {
     if (selectedItem === item) {
-        setSelectedItem(null); // Bỏ chọn nếu mục đã được chọn trước đó
-      } else {
-        setSelectedItem(item);
-      }
-      onPress && onPress(item);
+      setSelectedItem(null); // Bỏ chọn nếu mục đã được chọn trước đó
+    } else {
+      setSelectedItem(item);
+    }
+    onPress && onPress(item);
   };
   const RadioButtonSvg = (item) => {
     const uncheckedSvg = `<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -30,35 +29,58 @@ const ListAddress = ({ data, onPress }) => {
     return selectedItem === item ? checkedSvg : uncheckedSvg;
   };
   const _renderItem = ({ item, index }) => {
+    const addressKeys = Object.keys(item);
     return (
       <View>
-        <TouchableOpacity onPress={() => pressItem(item)}>
-          <View
-            style={{
-              flexDirection: "row",
+        {addressKeys.map((key, i) => {
+          if (key !== "id") {
+            const address = item[key];
+            return (
+              <TouchableOpacity key={i} onPress={() => pressItem(item)}>
+                <View
+                  style={{
+                    flexDirection: "row",
 
-              padding: 16,
-              borderBottomWidth: 0.5,
-              borderColor: "#D4D4D4",
-              justifyContent:'space-between'
-            }}
-          >
-            <View style={{flexDirection:'row'}}>
-                <TouchableOpacity onPress={() => pressItem(item)}>
-            <SvgXml xml={RadioButtonSvg(item)} />
-            </TouchableOpacity>
-            <View style={{ marginLeft: 12 }}>
-              <View style={{ flexDirection: "row" }}>
-                <Text>{item.name}</Text>
-                <Text style={{ marginHorizontal: 10 ,color:'#707070'}}>|</Text>
-                <Text style={{color:'#707070'}}>{item.phone_number}</Text>
-              </View>
-              <Text style={{fontSize:12,marginTop:8,color:'#707070'}}>{item.address}</Text>
-            </View>
-            </View>
-            <Text style={{fontSize:12,color:'#1890ff'}}>Sửa</Text>
-          </View>
-        </TouchableOpacity>
+                    padding: 16,
+                    borderBottomWidth: 0.5,
+                    borderColor: "#D4D4D4",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  <View
+                    key={i}
+                    style={{ flexDirection: "row", marginBottom: 8 }}
+                  >
+                    <TouchableOpacity onPress={() => pressItem(item)}>
+                      <SvgXml xml={RadioButtonSvg(item)} />
+                    </TouchableOpacity>
+                    <View style={{ marginLeft: 12 }}>
+                      <View style={{ flexDirection: "row" }}>
+                        <Text>{address.tennguoinhan}</Text>
+                        <Text
+                          style={{ marginHorizontal: 10, color: "#707070" }}
+                        >
+                          |
+                        </Text>
+                        <Text style={{ color: "#707070" }}>
+                          {address.sdtnguoinhan}
+                        </Text>
+                      </View>
+                      <Text
+                        style={{ fontSize: 12, marginTop: 8, color: "#707070" }}
+                      >
+                        {address.diachinhanhang}
+                      </Text>
+                    </View>
+                  </View>
+
+                  <Text style={{ fontSize: 12, color: "#1890ff" }}>Sửa</Text>
+                </View>
+                </TouchableOpacity>
+            );
+          }
+          return null;
+        })}
       </View>
     );
   };
