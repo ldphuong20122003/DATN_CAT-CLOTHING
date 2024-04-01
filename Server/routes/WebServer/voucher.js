5d024b36bfbdd2b2556345f1596e0ff8ce87e18c
Buicongthao <127530656+Buicongthao@users.noreply.github.com>
2024-04-02 00:28:58 +0700

    Revert "Devthao/editvoucher"

--- Server/routes/WebServer/voucher.js
+++ Server/routes/WebServer/voucher.js
@@ -6,6 +6,5 @@ var vouchers = require('../../controller/voucherController');
 
 router.get('/',vouchers.listVouchers);
 router.post('/delete/:id',vouchers.delete);
-router.post('/addVoucher',vouchers.addVoucher);
-router.post('/update/:id',vouchers.updateVoucher);
+
 module.exports = router;
