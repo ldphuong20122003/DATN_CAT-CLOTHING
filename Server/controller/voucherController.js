9582cfb0f1d25cc74ac89fe9292d932cd7464f2b
demonCthao <congthao1103@gmail.com>
2024-04-08 18:39:46 +0700

    chi edit được status đơn hàng

--- Server/controller/voucherController.js
+++ Server/controller/voucherController.js
@@ -1,27 +1,27 @@
 const admin = require('firebase-admin');
 // const firestore  = admin.firestore();
 // firestore.settings({ignoreUndefinedProperties:true});
-
+const firestore = admin.firestore();
 
 exports.listVouchers = async (req, res, next) => {
     let msg = '';
     let list = null;
     let listJson = [];
-  
-   
+
+
     try {
         list = await admin.firestore().collection('Vouchers').get();
-    // Truy vấn tài liệu từ Firestore
-    
+        // Truy vấn tài liệu từ Firestore
+
         list.docs.forEach(doc => {
             const docData = doc.data();
-           
+
             listJson.push(docData);
-           
-           
+
+
         });
 
-        
+
         msg = 'Lấy dữ liệu thành công !';
     } catch (error) {
         console.error('Error fetching data:', error);
@@ -29,44 +29,46 @@ exports.listVouchers = async (req, res, next) => {
         if (!res.headersSent) {
             res.status(500).send('Internal Server Error');
         }
-        
+
     }
 
     res.render('../views/Vou/listV.ejs', { title: "Vouchers", listVouchers: listJson });
 }
 
 
-exports.delete=async(req,res,next)=>{
-  try {
-      let id = req.params.id; // Lấy ID tài liệu từ URL
-  
-      // Xóa tài liệu dựa trên ID đã cung cấp
-      await admin.firestore().collection('Vouchers').doc(id).delete();
-  
-      res.redirect('/Vouchers');
+exports.delete = async (req, res, next) => {
+    try {
+        let id = req.params.id; // Lấy ID tài liệu từ URL
+
+        // Xóa tài liệu dựa trên ID đã cung cấp
+        await admin.firestore().collection('Vouchers').doc(id).delete();
+
+        res.redirect('/Vouchers');
     } catch (error) {
-      console.error('Error deleting data:', error);
-      res.status(500).send('Error deleting data from Firestore');
+        console.error('Error deleting data:', error);
+        res.status(500).send('Error deleting data from Firestore');
     }
 }
 
 exports.addVoucher = async (req, res, next) => {
-  
+
+
     try {
-        var addlist = null;
-        addlist =  await admin.firestore().collection('Vouchers').add({
-            Discount: req.body.Discount,
-            id: req.body.id,
+
+        const addlist = await firestore.collection('Vouchers').add({
+            Discount: req.body.discount,
+            id: req.body.voucherid,
             Title: req.body.Title,
             From: req.body.from
         });
-        console.log('danhsachvoucher:',addlist);
+        console.log('danhsachvoucher:', addlist.Title);
         res.redirect('/Vouchers')
     } catch (error) {
         console.error('Error adding data:', error);
         res.status(500).send('Error adding data from Firestore');
+
     }
-    
+
 };
 
 exports.updateVoucher = async (req, res, next) => {
@@ -74,7 +76,7 @@ exports.updateVoucher = async (req, res, next) => {
     try {
         await admin.firestore().collection('Vouchers').doc(_id).set({
 
-            Discount: req.body.Discount,
+            Discount: req.body.discount,
             id: req.body.id,
             Title: req.body.Title,
             From: req.body.from
